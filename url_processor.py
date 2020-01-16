c41fe1a72baaec95f490e1acb257a245ff41c7e7
Qing Wang <qwqwqw@gmail.com>
2020-01-15 18:23:20 -0800

    black format

--- url_processor.py
+++ url_processor.py
@@ -6,9 +6,10 @@ Author: Qing Wang
 """
 
 import re
-LC_LIST = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
-CAP_LIST = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
-NUM_LIST = ['0', '1', '2', '3', '4', '5', '6']
+
+LC_LIST = ["a", "b", "c", "d", "e", "f", "g"]
+CAP_LIST = ["A", "B", "C", "D", "E", "F", "G"]
+NUM_LIST = ["0", "1", "2", "3", "4", "5", "6"]
 
 
 class URLProcessor(object):
@@ -23,13 +24,13 @@ class URLProcessor(object):
 
     def _generate_template(self, url):
         """Generate the template string from url."""
-        fn = url.split('/')[-1]
-        str_to_replaced = re.findall(r'\d+', fn)
+        fn = url.split("/")[-1]
+        str_to_replaced = re.findall(r"\d+", fn)
         self.num_vars = len(str_to_replaced)
         self.n_digits = [len(s) for s in str_to_replaced]
         rep = {}
         for index, item in enumerate(str_to_replaced):
-            rep[item] = '{var%i}' % index
+            rep[item] = "{var%i}" % index
         # use these three lines to do the replacement
         rep = dict((re.escape(k), v) for k, v in rep.items())
         pattern = re.compile("|".join(rep.keys()))
@@ -39,18 +40,28 @@ class URLProcessor(object):
     def normal_url_list(self):
         """Generate normal url list for iteration."""
         for i in range(0, self.pnum + 1):
-            rep_dict = {'var%i' % t: str(i).zfill(self.n_digits[t])
-                        for t in range(self.num_vars)}
+            rep_dict = {
+                "var%i" % t: str(i).zfill(self.n_digits[t])
+                for t in range(self.num_vars)
+            }
             yield self.template.format(**rep_dict)
 
-    def special_url_list(self, sep=''):
+    def special_url_list(self, sep=""):
         """Generate special urls for iteration."""
         sp_c_list = LC_LIST + CAP_LIST + NUM_LIST
         for c in sp_c_list:
             if sep:
-                rep_dict = {'var%i' % t: '0'.zfill(self.n_digits[t]) if t < self.num_vars - 1 else '0'.zfill(self.n_digits[t]) + sep + c
-                            for t in range(self.num_vars)}
+                rep_dict = {
+                    "var%i" % t: "0".zfill(self.n_digits[t])
+                    if t < self.num_vars - 1
+                    else "0".zfill(self.n_digits[t]) + sep + c
+                    for t in range(self.num_vars)
+                }
             else:
-                rep_dict = {'var%i' % t: '0'.zfill(self.n_digits[t]) if t < self.num_vars - 1 else '0'.zfill(self.n_digits[t]) + c
-                            for t in range(self.num_vars)}
+                rep_dict = {
+                    "var%i" % t: "0".zfill(self.n_digits[t])
+                    if t < self.num_vars - 1
+                    else "0".zfill(self.n_digits[t]) + c
+                    for t in range(self.num_vars)
+                }
             yield self.template.format(**rep_dict)
