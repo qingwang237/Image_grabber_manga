6706b566865dda6a23496776cffa14d6c5e6e299
Qing Wang <qwqwqw@gmail.com>
2020-01-15 18:15:36 -0800

    black format

--- image_grabber.py
+++ image_grabber.py
@@ -21,7 +21,7 @@ class ImageGrabber(object):
         """The constructor func."""
         self.url = start_url
         self.base_path = base_path
-        self.base_url = "https://" + (self.url.split('/'))[-2]
+        self.base_url = "https://" + (self.url.split("/"))[-2]
         self.mode = mode
         self.validate()
 
@@ -29,8 +29,8 @@ class ImageGrabber(object):
         """Get the data url from passed url."""
         url = self.base_url + next_url
         r = requests.get(url)
-        soup = BeautifulSoup(r.content, 'lxml')
-        src = soup.find("img", {"id": "picarea"})['src']
+        soup = BeautifulSoup(r.content, "lxml")
+        src = soup.find("img", {"id": "picarea"})["src"]
         return src
 
     def validate(self):
@@ -38,25 +38,23 @@ class ImageGrabber(object):
         if self.url:
             self.result = requests.get(self.url)
             if self.result.status_code == 200:
-                soup = BeautifulSoup(self.result.content, 'lxml')
+                soup = BeautifulSoup(self.result.content, "lxml")
                 try:
                     self.title = soup.find_all("h2")[0].string.strip()
                 except IndexError:
                     print("Please make sure the url is correct.")
                     self.valid = False
                     return
-                link = soup.find_all("div", {"class": "pic_box"})[-1].find('a')
+                link = soup.find_all("div", {"class": "pic_box"})[-1].find("a")
                 # also save the first link
-                self.img_link = soup.find("div", {"class": "pic_box"}).find('a')['href']
+                self.img_link = soup.find("div", {"class": "pic_box"}).find("a")["href"]
                 if link:
-                    self.data_url = self._url_resolver(link['href'])
-                    patten = re.compile(r'頁數')
-                    pages = re.findall(
-                        r'\d+', soup.find("label", text=patten).string)
+                    self.data_url = self._url_resolver(link["href"])
+                    patten = re.compile(r"頁數")
+                    pages = re.findall(r"\d+", soup.find("label", text=patten).string)
                     self.page_num = int(pages[0])
                     # find the catagory and lang tags
-                    tags = soup.find(
-                        "div", {"class": "png bread"}).find_all('a')
+                    tags = soup.find("div", {"class": "png bread"}).find_all("a")
                     if tags[1].string == "單行本":
                         self.tag = "volume"
                     elif tags[1].string == "雜誌&短篇":
@@ -77,7 +75,7 @@ class ImageGrabber(object):
                         else:
                             self.subtag = "unknown"
                     except IndexError:
-                            self.subtag = "unknown"
+                        self.subtag = "unknown"
                     self.valid = True
                 else:
                     print("Cannot find data url.")
@@ -91,16 +89,19 @@ class ImageGrabber(object):
 
     def _base_path_modifier(self):
         """Generate the new path based on the tags."""
-        return self.base_path + self.tag + '/' + self.subtag + '/'
+        return self.base_path + self.tag + "/" + self.subtag + "/"
 
     def _page_crawl(self, start):
         """The page crawler iterator."""
         url = self.base_url + start
         for i in range(self.page_num):
             result = requests.get(url)
-            soup = BeautifulSoup(result.content, 'lxml')
-            img_url = soup.find('span', {'id': 'imgarea'}).find('a').find('img')['src']
-            url = self.base_url + soup.find('div', {'class': 'newpage'}).find_all('a')[-1]['href']
+            soup = BeautifulSoup(result.content, "lxml")
+            img_url = soup.find("span", {"id": "imgarea"}).find("a").find("img")["src"]
+            url = (
+                self.base_url
+                + soup.find("div", {"class": "newpage"}).find_all("a")[-1]["href"]
+            )
             yield img_url
 
     def _download_list(self, iter_list):
@@ -111,17 +112,17 @@ class ImageGrabber(object):
                 file_url = self.base_url + url
                 r = requests.get(file_url)
                 if r.status_code == 404:
-                    if file_url.split('.')[-1] == 'jpg':
-                        file_url = file_url.replace('jpg', 'png')
+                    if file_url.split(".")[-1] == "jpg":
+                        file_url = file_url.replace("jpg", "png")
                     else:
-                        file_url = file_url.replace('png', 'jpg')
+                        file_url = file_url.replace("png", "jpg")
                     r = requests.get(file_url)
                 elif r.status_code == 200:
                     try:
                         img = Image.open(BytesIO(r.content))
-                        img.save(new_folder + '/' + file_url.split('/')[-1])
+                        img.save(new_folder + "/" + file_url.split("/")[-1])
                     except OSError:
-                        print(file_url + '  cannot be saved.')
+                        print(file_url + "  cannot be saved.")
                 else:
                     pass
 
@@ -133,22 +134,20 @@ class ImageGrabber(object):
         except OSError:
             pass
         # handle normal image naming rules
-        if self.mode == 'crawl':
+        if self.mode == "crawl":
             self._download_list(self._page_crawl(self.img_link))
         else:
             url_parsed = URLProcessor(self.data_url, self.page_num)
             self._download_list(url_parsed.normal_url_list())
             self._download_list(url_parsed.special_url_list())
-            self._download_list(url_parsed.special_url_list(sep='-'))
-            self._download_list(url_parsed.special_url_list(sep='_'))
+            self._download_list(url_parsed.special_url_list(sep="-"))
+            self._download_list(url_parsed.special_url_list(sep="_"))
 
 
 @click.command()
-@click.option('--url', help='The starting url of the manga.')
-@click.option('--folder', default='~/Hmanga/',
-              help='The folder to save manga.')
-@click.option('--mode', default='crawl',
-              help='The mode for downloading')
+@click.option("--url", help="The starting url of the manga.")
+@click.option("--folder", default="~/Hmanga/", help="The folder to save manga.")
+@click.option("--mode", default="crawl", help="The mode for downloading")
 def downloader(url, folder, mode):
     """The main func."""
     path = expanduser(folder)
@@ -165,5 +164,5 @@ def downloader(url, folder, mode):
         print("The start url is not recognized.")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     downloader()
