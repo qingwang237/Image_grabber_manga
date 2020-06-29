4a0fb4011372e5d4c55db8b222b49cc258af7c1d
Qing Wang <qwqwqw@gmail.com>
2020-06-28 17:57:40 -0700

    more docstring format

--- wgrabber/url_processor.py
+++ wgrabber/url_processor.py
@@ -13,17 +13,23 @@ NUM_LIST = ["0", "1", "2", "3", "4", "5", "6"]
 
 
 class URLProcessor(object):
-    """Class for URLProcessor."""
+    """
+    Class for URLProcessor.
+    """
 
     def __init__(self, data_url, page_num):
-        """Constructor method."""
+        """
+        Constructor method.
+        """
         super(URLProcessor, self).__init__()
         self.pnum = page_num
         self.data_url = data_url
         self.template = self._generate_template(self.data_url)
 
     def _generate_template(self, url):
-        """Generate the template string from url."""
+        """
+        Generate the template string from url.
+        """
         fn = url.split("/")[-1]
         str_to_replaced = re.findall(r"\d+", fn)
         self.num_vars = len(str_to_replaced)
@@ -38,7 +44,9 @@ class URLProcessor(object):
         return text
 
     def normal_url_list(self):
-        """Generate normal url list for iteration."""
+        """
+        Generate normal url list for iteration.
+        """
         for i in range(0, self.pnum + 1):
             rep_dict = {
                 "var%i" % t: str(i).zfill(self.n_digits[t])
@@ -47,7 +55,9 @@ class URLProcessor(object):
             yield self.template.format(**rep_dict)
 
     def special_url_list(self, sep=""):
-        """Generate special urls for iteration."""
+        """
+        Generate special urls for iteration.
+        """
         sp_c_list = LC_LIST + CAP_LIST + NUM_LIST
         for c in sp_c_list:
             if sep:
